a2a3a82875a7dd749c2cab40917adce21f959378
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2022-02-27 07:42:18 +0900

    Rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_12_11_141402) do
-
+ActiveRecord::Schema[7.0].define(version: 2017_12_11_141402) do
   create_table "advent_calendar_items", force: :cascade do |t|
     t.string "user_name"
     t.string "comment"
     t.integer "date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.integer "user_id"
     t.integer "event_id"
   end
@@ -25,8 +24,8 @@ ActiveRecord::Schema.define(version: 2017_12_11_141402) do
   create_table "attachments", force: :cascade do |t|
     t.integer "advent_calendar_item_id"
     t.string "image"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["advent_calendar_item_id"], name: "index_attachments_on_advent_calendar_item_id"
   end
 
@@ -34,8 +33,8 @@ ActiveRecord::Schema.define(version: 2017_12_11_141402) do
     t.integer "item_id"
     t.string "user_name"
     t.text "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["item_id"], name: "index_comments_on_item_id"
   end
 
@@ -47,16 +46,16 @@ ActiveRecord::Schema.define(version: 2017_12_11_141402) do
     t.date "end_date"
     t.integer "created_by_id"
     t.integer "updated_by_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.text "description"
   end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.integer "advent_calendar_item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.integer "comments_count", default: 0, null: false
     t.text "body"
     t.index ["advent_calendar_item_id"], name: "index_items_on_advent_calendar_item_id", unique: true
@@ -65,30 +64,30 @@ ActiveRecord::Schema.define(version: 2017_12_11_141402) do
   create_table "likes", force: :cascade do |t|
     t.integer "item_id"
     t.integer "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "locked_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "name"
     t.boolean "admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
