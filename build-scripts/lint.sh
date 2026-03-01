ca79ec1f4a29d8f723e1e8a9c74494efdf4474f9
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-03-01 04:25:21 +0000

    feat: enhance YARD documentation check to ensure 100% coverage

--- build-scripts/lint.sh
+++ build-scripts/lint.sh
@@ -11,7 +11,14 @@ echo "Running RuboCop..."
 bundle exec rubocop --parallel
 
 echo "Running YARD documentation check..."
-bundle exec yard stats --list-undoc
+set +e
+bundle exec yard stats --list-undoc --no-save | grep "100.00%" > /dev/null
+if [ $? -ne 0 ]; then
+  echo "YARD documentation coverage is below 100%. See report above." >&2
+  exit 1
+fi
+set -e
+echo "All objects are documented"
 
 echo "Running ESLint..."
 yarn lint
