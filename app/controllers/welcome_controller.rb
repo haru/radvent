83636e08685fcf8c497c027912429a0bb9c7c33d
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-03-04 11:10:14 +0000

    feat: refactor welcome controller and update board display logic in views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,8 @@ class WelcomeController < ApplicationController
   #
   # @return [void]
   def index
-    @events = Board.find_by!(board_type: :top).events.order(start_date: :desc)
+    @board = Board.find_by!(board_type: :top)
+    @events = @board.events.order(start_date: :desc)
+    render 'boards/show'
   end
 end
