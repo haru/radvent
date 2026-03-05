5398d47df8e66a98b3b4b8eb5582c573e1d2f225
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-03-05 12:53:16 +0000

    feat: remove redundant index creation in migration for boards and board memberships

--- db/migrate/20260305123019_add_unique_indexes_to_boards_events_and_board_memberships.rb
+++ db/migrate/20260305123019_add_unique_indexes_to_boards_events_and_board_memberships.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class AddUniqueIndexesToBoardsEventsAndBoardMemberships < ActiveRecord::Migration[8.1]
-  def change
-    add_index :boards, :board_id, unique: true
-    add_index :board_memberships, %i[user_id board_id], unique: true
-  end
+  # Indexes already created in CreateBoards and CreateBoardMemberships migrations
+  def change; end
 end
