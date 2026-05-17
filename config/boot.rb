15443fb3ece6b6e7f0383fddfb5358ec3455113b
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-05-17 12:07:16 +0000

    style: format Bootsnap setup for consistency

--- config/boot.rb
+++ config/boot.rb
@@ -5,9 +5,9 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap"
 Bootsnap.setup(
-  cache_dir:          "#{__dir__}/../tmp/cache",
-  development_mode:   ENV['RAILS_ENV'] == 'development',
-  load_path_cache:    true,
+  cache_dir: "#{__dir__}/../tmp/cache",
+  development_mode: ENV['RAILS_ENV'] == 'development',
+  load_path_cache: true,
   compile_cache_iseq: !(defined?(Coverage) && Coverage.running?),
-  compile_cache_yaml: true,
+  compile_cache_yaml: true
 )
